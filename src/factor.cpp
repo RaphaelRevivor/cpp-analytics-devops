68f7d816f2bd3ca4560769dc5df250901bc451ec
RaphaelRevivor <marvelous.islander@gmail.com>
2025-09-29 21:54:03 +0200

    Only output prime numbers in the generated factorization vector (#2)

--- src/factor.cpp
+++ src/factor.cpp
@@ -2,43 +2,64 @@
 
 using namespace std;
 
-// === member functions for PollardsPho ===
+// === member functions for Factorization ===
 /// @brief constructor
 /// @param n the value for fatorization
-PollardsPho::PollardsPho(int64_t n) : n(n)
+Factorization::Factorization(int64_t n) : n(n)
 {
   srand(time(0));
 }
 
+vector<int64_t> Factorization::getAllFactors()
+{
+  vector<int64_t> output = {};
+  int64_t tmp = n;
+  while(tmp != 1)
+  {
+    auto factor = getFactor(tmp);
+    output.emplace_back(factor);
+    tmp = tmp / factor;
+  }
+  return output;
+}
+
 /// @brief the main function to do the calculation
 /// @return result
-int64_t PollardsPho::calculate()
+int64_t Factorization::getFactor(const int64_t& input)
 {
   int64_t d = 1;
   int64_t cnt = 0;
-  int64_t x = selectX0();
+  int64_t x = selectX0(input);
   int64_t y = x;
 
   // trivial cases
-  if (n == 1 || n == 2 || n == 3)
-    return n;
-  else if (n%2 == 0)
-    return 2;
-  // check prime numbers
-  else if (millerRabin())
-    return n;
-
-  while(d == 1 || d == n)
+  if (input == 1 || input == 2 || input == 3)
+    return input;
+  else if (input%2 == 0)
   {
-    d = basicIteration(x, y);
-    if (d == n)
+    return 2;
+  }
+  // check prime numbers
+  else if (millerRabin(input))
+  {
+    return input;
+  }   
+
+  // must have a factor
+  while(d == 1 || d == input)
+  {
+    d = pollardsPho(x, y, input);
+    if (d == input)
     {
       failure++;
     }
-
-    basicLogging(x, y, d, cnt);
-
+    // basicLogging(x, y, d, cnt);
     cnt++;
+  }
+
+  if (!millerRabin(d))
+  {
+    d = getFactor(d);
   }
 
   return d;
@@ -48,42 +69,42 @@ int64_t PollardsPho::calculate()
 /// @param x x
 /// @param y y
 /// @return d
-int64_t PollardsPho::basicIteration(int64_t &x, int64_t &y)
+int64_t Factorization::pollardsPho(int64_t &x, int64_t &y, const int64_t& input)
 {
   selectC(c);
-  x = squareMod(x, c);
-  y = squareMod(squareMod(y, c), c);
-  int64_t d = calculateD(x, y);
-
-  return d;
+  x = squareMod(x, c, input);
+  y = squareMod(squareMod(y, c, input), c, input);
+  return calculateD(x, y, input);
 }
 
 /// @brief  select the initial x
 /// @return x
-int64_t PollardsPho::selectX0()
+int64_t Factorization::selectX0(const int64_t& input)
 {
-  return (rand() % (n - 2)) + 2;
+  
+  int64_t output = (rand() % (input - 2)) + 2;
+  return output;
 }
 
 /// @brief function to calculate f(x) = x^2 + c
 /// @param x x
 /// @param c c
 /// @return f(x)
-int64_t PollardsPho::squareMod(int64_t x, int64_t c)
+int64_t Factorization::squareMod(const int64_t& x, const int64_t& c, const int64_t& input)
 {
-  __int128_t output = (x * x + c) % n;
+  __int128_t output = (x * x + c) % input;
   return output;
 }
 
 /// @brief funtion to calculate D = gcd(abs(x-y), n)
-int64_t PollardsPho::calculateD(int64_t x, int64_t y)
+int64_t Factorization::calculateD(const int64_t& x, const int64_t& y, const int64_t& input)
 {
-  return gcd(abs(x-y), n);
+  return gcd(abs(x-y), input);
 }
 
 /// @brief function to select c
 /// @return c
-void PollardsPho::selectC(int64_t& input)
+void Factorization::selectC(int64_t& input)
 {
   if (failure < 100)
     input = 1;
@@ -101,17 +122,17 @@ void PollardsPho::selectC(int64_t& input)
 /// @param y y
 /// @param d d
 /// @param cnt cnt
-void PollardsPho::basicLogging(int64_t &x, int64_t &y, int64_t &d, int64_t &cnt)
+void Factorization::basicLogging(int64_t &x, int64_t &y, int64_t &d, int64_t &cnt)
 {
   cout << cnt << ":" << endl;
-  cout << "x, y, d:" << x << "," << y << "," << d << endl;
+  cout << "x, y, d: " << x << "," << y << "," << d << endl;
 }
 
 /// @brief function to check prime (in the cases of none 1, 2, 3 or even)
 /// @return bool result
-bool PollardsPho::millerRabin()
+bool Factorization::millerRabin(const int64_t& input)
 {
-  int64_t d = n - 1;
+  int64_t d = input - 1;
   int64_t s = 0;
   while(d % 2 == 0)
   {
@@ -123,20 +144,20 @@ bool PollardsPho::millerRabin()
   vector<int64_t> bases = {2,3,5,7,11,13,17};
   for(const auto& base : bases)
   {
-    if (base >= n)
+    if (base >= input)
       continue;
 
     // calculate x = a^d % n
-    int64_t x = mod_pow(base, d);
+    __int128_t x = mod_pow(base, d, input);
 
-    if (x == 1 || x == n - 1)
+    if (x == 1 || x == input - 1)
       continue;
     
     bool passed = false;
     for(int64_t i = 1; i < s; i++)
     {
-      x = x * x % n;
-      if (x == n - 1)
+      x = x * x % input;
+      if (x == input - 1)
       {
         passed = true;
         break;
@@ -152,18 +173,18 @@ bool PollardsPho::millerRabin()
 /// @param b b
 /// @param d d
 /// @return mod power result
-int64_t PollardsPho::mod_pow(int64_t b, int64_t d)
+int64_t Factorization::mod_pow(const int64_t& b, const int64_t& d, const int64_t& input)
 {
   __int128_t result = 1;
-  b = b % n;
-  while(d > 0)
+  int64_t base = b;
+  int64_t dd = d;
+  base = base % input;
+  while(dd > 0)
   {
-    if (d % 2 == 1)
-      result = (result * b) % n;
-    b = b * b % n;
-    d /= 2;
+    if (dd % 2 == 1)
+      result = (result * base) % input;
+    base = base * base % input;
+    dd /= 2;
   }
   return result;
 }
-
-// === member functions for Factorization ===
--- src/factor.h
+++ src/factor.h
@@ -9,36 +9,29 @@
 #include <cstdlib>
 #include <numeric>
 
-class PollardsPho
+using namespace std;
+
+class Factorization
 {
 public:
-  PollardsPho(int64_t n);
-  ~PollardsPho() = default;
-  int64_t calculate();
+  Factorization(int64_t n);
+  ~Factorization() = default;
+  vector<int64_t> getAllFactors();
+  int64_t getFactor(const int64_t& input);
 
 private:
   int64_t n;
   int64_t c;
   int64_t failure = 0;
-  int64_t squareMod(int64_t x, int64_t c);
-  int64_t calculateD(int64_t x, int64_t y);
+  vector<int64_t> factors = {};
+  int64_t squareMod(const int64_t& x, const int64_t& c, const int64_t& input);
+  int64_t calculateD(const int64_t& x, const int64_t& y, const int64_t& input);
   void selectC(int64_t& input);
-  int64_t selectX0();
-  int64_t basicIteration(int64_t &x, int64_t &y);
-  bool millerRabin();
-  int64_t mod_pow(int64_t b, int64_t d);
+  int64_t selectX0(const int64_t& input);
+  int64_t pollardsPho(int64_t &x, int64_t &y, const int64_t& input);
+  bool millerRabin(const int64_t& input);
+  int64_t mod_pow(const int64_t& b, const int64_t& d, const int64_t& input);
   void basicLogging(int64_t &x, int64_t &y, int64_t &d, int64_t &cnt);
 };
-
-// class Factorization
-// {
-// public:
-//   Factorization(int64_t n) : n(n)
-//   {
-//   }
-//   ~Factorization() = default;
-// private:
-//   int64_t n;
-// };
 
 #endif
